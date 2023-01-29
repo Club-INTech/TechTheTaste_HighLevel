b59b2d2fd966404f361eaae2da5dcc9b6faa08ac
EKnight-Blue <89185062+EKnight-Blue@users.noreply.github.com>
2023-01-29 17:30:17 +0100

    Update __init__.py

--- src/microProcess/controller/__init__.py
+++ src/microProcess/controller/__init__.py
@@ -3,6 +3,10 @@ from controller.controller_classes import ControllerButtons, ControllerMouse
 
 
 class Controller:
+    """
+    Tool to get events from a ps4 controller, works on UNIX
+    """
+    
     def __init__(self, joystick_file='/dev/input/js0', mouse_file='/dev/input/mouse0'):
         self.queue = SimpleQueue()
         self.controllerButtonsProcess = Process(target=ControllerButtons(self.queue, joystick_file).mainloop)
