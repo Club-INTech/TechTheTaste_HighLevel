ef7c539179d3ed6c8a4a95fcf6e9099d50f29042
EKnight-Blue <89185062+EKnight-Blue@users.noreply.github.com>
2023-01-29 17:34:57 +0100

    Update constants.py

--- src/microProcess/controller/constants.py
+++ src/microProcess/controller/constants.py
@@ -1,6 +1,3 @@
-
-EVENT_FORMAT = "Event<type={}, button={}, value={}>"
-
 EVENT_TYPES = 'UNKNOWN', 'DIGITAL', 'ANALOG', 'PAD'
 OTHER_TYPE, DIGITAL, ANALOG, PAD = range(4)
 
@@ -8,7 +5,6 @@ DIGITAL_BUTTONS = (
     'x', 'o', 'Δ', '□', 'L1', 'R1', 'L2', 'R2', 'Share', 'Options', 'Playstation', 'L3', 'R3'
 )
 CROSS, CIRCLE, TRIANGLE, SQUARE, L1, R1, L2_D, R2_D, SHARE, OPTIONS, PS, L3, R3 = range(13)
-
 
 ANALOG_BUTTONS = (
     'Lx', 'Ly', 'L2', 'Rx', 'Ry', 'R2', 'H_arrows', 'V_arrows'
@@ -23,7 +19,8 @@ FREE_PAD, CLICKED_PAD = range(2)
 
 BUTTON_NAMES = DIGITAL_BUTTONS, ANALOG_BUTTONS, PAD_STATES
 
+# Used to display Events
+EVENT_FORMAT = "Event<type={}, button={}, value={}>"
 BUTTONS = {
     (ty, bu): name for ty, buttons in enumerate(BUTTON_NAMES, 1) for bu, name in enumerate(buttons)
 }
-
