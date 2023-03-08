b612f66e10221ba81a5da772d1a1c25a7d870c1f
EKnight-Blue <loicchechevalier@gmail.com>
2023-03-08 22:19:12 +0100

    new var

--- src/microProcess/constants.py
+++ src/microProcess/constants.py
@@ -47,7 +47,7 @@ WHEEL_RADIUS = .034  # in m, +- 0.1mm, measurements 18/02/2023 ~17h coupling bot
 # PID variables
 VAR_NAMES = (
     'Kp_right', 'Kd_right', 'Ki_right', 'Kp_left', 'Kd_left', 'Ki_left', 'Kp_trans', 'Kd_trans', 'Ki_trans',
-    'Kp_rot', 'Kd_rot', 'Ki_rot'
+    'Kp_rot', 'Kd_rot', 'Ki_rot', 'vitesse'
 )
 VAR_DICT = {n: i for i, n in enumerate(VAR_NAMES)}
 
