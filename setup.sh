d67463adaae60ea66cfbf3be2d33c9d1f225278d
VasaOne <derrien.maxime77@gmail.com>
2023-03-18 05:36:13 +0100

    corrected small bug with log dir

--- setup.sh
+++ setup.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+mkdir log
+touch log/com1.txt
+touch log/com2.txt
+touch log/lidar.txt
+touch log/lpa.txt
+touch log/main.txt
+
 echo 'starting setup INTech CDR 2022 2023'
 
 python3 -m venv ./venv
