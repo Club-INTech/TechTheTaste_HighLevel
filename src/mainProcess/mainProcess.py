1b40c3aa946e746e62e3af14bfe20d036aeee280
Henri NGO <henringo@Henri-NGO-5.local>
2023-04-11 02:03:38 +0200

    test

--- src/mainProcess/mainProcess.py
+++ src/mainProcess/mainProcess.py
@@ -17,6 +17,7 @@ class mainProcess:
         #scenario.debugLidar(self.pipeMainToMicro1, self.pipeMainToMicro2, self.pipeMainToLPA, self.pipeLidarToMain)
         #scenario.debugRaspy(self.pipeMainToMicro1, self.pipeMainToMicro2, self.pipeMainToLPA)
         
+        #test
         scenario.debugSimpleOrderStraight1(self.pipeMainToMicro1, self.pipeMainToMicro2, self.pipeMainToLPA)
         #scenario.debugSimpleOrderStraight2(self.pipeMainToMicro1, self.pipeMainToMicro2, self.pipeMainToLPA)
         #scenario.debugSimpleOrderStraight3(self.pipeMainToMicro1, self.pipeMainToMicro2, self.pipeMainToLPA)
