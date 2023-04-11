295f674b5d6296c9684e0656bbc739603fee4bea
Henri NGO <henringo@Henri-NGO-5.local>
2023-04-11 04:00:38 +0200

    test scenario turn

--- src/mainProcess/mainProcess.py
+++ src/mainProcess/mainProcess.py
@@ -18,13 +18,13 @@ class mainProcess:
         #scenario.debugRaspy(self.pipeMainToMicro1, self.pipeMainToMicro2, self.pipeMainToLPA)
         
         #scenario.debugSimpleOrderStraight1(self.pipeMainToMicro1, self.pipeMainToMicro2, self.pipeMainToLPA)
-        scenario.debugSimpleOrderStraight2(self.pipeMainToMicro1, self.pipeMainToMicro2, self.pipeMainToLPA)
+        #scenario.debugSimpleOrderStraight2(self.pipeMainToMicro1, self.pipeMainToMicro2, self.pipeMainToLPA)
         #scenario.debugSimpleOrderStraight3(self.pipeMainToMicro1, self.pipeMainToMicro2, self.pipeMainToLPA)
         #scenario.debugSimpleOrderStraight4(self.pipeMainToMicro1, self.pipeMainToMicro2, self.pipeMainToLPA)
 
         #scenario.debugSimpleOrderDiagonal(self.pipeMainToMicro1, self.pipeMainToMicro2, self.pipeMainToLPA)
         
-        #scenario.debugSimpleOrderTurn(self.pipeMainToMicro1, self.pipeMainToMicro2, self.pipeMainToLPA)
+        scenario.debugSimpleOrderTurn(self.pipeMainToMicro1, self.pipeMainToMicro2, self.pipeMainToLPA)
         
         #scenario.debugSimpleOrderCaptureCakeMid(self.pipeMainToMicro1, self.pipeMainToMicro2, self.pipeMainToLPA
         #scenario.debugSimpleOrderCaptureCakeRight(self.pipeMainToMicro1, self.pipeMainToMicro2, self.pipeMainToLPA)
