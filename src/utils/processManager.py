852a9ffe57b31e8b3ae0c8575e15358e1b8a9384
EKnight-Blue <loicchechevalier@gmail.com>
2023-04-14 12:37:39 +0200

    value

--- src/utils/processManager.py
+++ src/utils/processManager.py
@@ -2,8 +2,8 @@ import log
 from time import sleep
 from multiprocessing import Pipe, Process
 
-port1 = "/dev/ttyUSB1"
-port2 = '/dev/ttyUSB0'
+port1 = "/dev/ttyUSB0"
+port2 = '/dev/ttyUSB1'
 
 # we wait X and Y initialized to start the other process
 def isXYinitialised(XYinitialised):
