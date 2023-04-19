63d00e59057aea9bc662410bafbb171f33d1fcad
Henri NGO <henringo@Henri-NGO-5.local>
2023-04-19 21:50:10 +0200

    fixing log

--- run.sh
+++ run.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 
-echo '----------start robot------------------'
+echo '---------------------------------- start robot ----------------------------------'
 echo  $1
 
-
-echo '-------------activate venv-------------------'
+echo '--------------------------------- activate venv ---------------------------------'
+echo  $1
 source ./venv/bin/activate
 
 # should add something for the version of the robot
-
-echo '----------------start of python script-------------'
+echo '----------------------------- start of python script ----------------------------'
 python3 src/mainProcess/launcher.py
 
 
