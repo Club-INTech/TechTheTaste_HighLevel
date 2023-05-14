93db157436c5b88b30c381a0ce147558818f8a0d
EKnight-Blue <loicchechevalier@gmail.com>
2023-05-14 23:28:43 +0200

    test

--- src/microProcess/no_main_process.py
+++ src/microProcess/no_main_process.py
@@ -114,7 +114,7 @@ if __name__ == '__main__':
     
     main_ = mp.Process(target=main_process, args=(main_pipe0,))
     lidar_ = mp.Process(target=lidar_process, args=(lidar_pipe1, r, color))
-    micro_ = mp.Process(target=MicroProcess, args=(lidar_pipe0, main_pipe1, r, True))
+    micro_ = mp.Process(target=MicroProcess, args=(lidar_pipe0, main_pipe1, r, False))
 
     lidar_.start()
     main_.start()
