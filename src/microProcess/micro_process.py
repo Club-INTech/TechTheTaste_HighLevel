42075506f4f8e4f93cf7972b4ef89c74002ab0d9
EKnight-Blue <loicchechevalier@gmail.com>
2023-05-14 23:15:49 +0200

    test

--- src/microProcess/micro_process.py
+++ src/microProcess/micro_process.py
@@ -137,7 +137,7 @@ class MPMovement(MovementMicro, MPGenericMicro):
         if d_left == d_right:
             self.master.robot.x += left_arc * math.cos(self.master.robot.h)
             self.master.robot.y += left_arc * math.sin(self.master.robot.h)
-            print(f'\rOdometry: {self.master.robot.x} {self.master.robot.y} {self.master.robot.h}', end='')
+            # print(f'\rOdometry: {self.master.robot.x} {self.master.robot.y} {self.master.robot.h}', end='')
             return
 
         # circular movement
@@ -148,7 +148,7 @@ class MPMovement(MovementMicro, MPGenericMicro):
         self.master.robot.y += radius * (math.sin(a1) - math.sin(a0))
         self.master.robot.h += angle
 
-        print(f'\rOdometry: {self.master.robot.x} {self.master.robot.y} {self.master.robot.h}', end='')
+        # print(f'\rOdometry: {self.master.robot.x} {self.master.robot.y} {self.master.robot.h}', end='')
 
 
 class MPAction(ActionMicro, MPGenericMicro):
