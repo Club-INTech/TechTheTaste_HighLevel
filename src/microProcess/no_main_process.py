dece8badc68f8ce91699fccc06fd7ca80a012b1a
EKnight-Blue <loicchechevalier@gmail.com>
2023-05-14 19:57:27 +0200

    Cleaning

--- src/microProcess/no_main_process.py
+++ src/microProcess/no_main_process.py
@@ -11,40 +11,9 @@ import lidarProcess
 
 
 def lidar_process(pipe, r, color):
-    lidar=lidarProcess.Lili()
+    lidar = lidarProcess.Lili()
     lidar.lidarstop(pipe)                           #simple lidar
     #lidar.lidarstop2(pipe, r.x, r.y, r.h, start)   #with track limitation
-
-
-def move():
-    yield MOV, 0, 2000
-    yield ROT, 0, 200
-    yield ROT, 0, -200 + 0x10000
-    yield MOV, 0, -2000 + 0x10000
-
-
-def vert(dv):
-    n_dv = -dv
-    dv += 0x10000 * (dv < 0)
-    n_dv += 0x10000 * (n_dv < 0)
-    return ARM, 0, (n_dv << 16) | dv
-
-
-def horiz(dh):
-    dh += 0x10000 * (dh < 0)
-    return ARM, 0, (dh << 16) | dh
-
-
-def arm():
-    yield horiz(-700)
-    yield vert(-550)
-    yield PUM, 1, 1
-    yield vert(550)
-    yield horiz(-700)
-    yield vert(-550)
-    yield PUM, 1, 0
-    yield vert(550)
-    yield horiz(1400)
 
 
 lidar_pipe0, lidar_pipe1 = mp.Pipe()
