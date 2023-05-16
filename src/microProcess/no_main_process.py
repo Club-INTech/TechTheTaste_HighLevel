26ee6d4b4671a6b0560ee7e0aaaed43cf15e67de
EKnight-Blue <loicchechevalier@gmail.com>
2023-05-17 00:46:55 +0200

    tests

--- src/microProcess/no_main_process.py
+++ src/microProcess/no_main_process.py
@@ -125,11 +125,12 @@ def main_process(pipe):
     s.append(RobotAction(r, MOVEMENT, 'goto', 1.75, -.1))
     s.append(RobotAction(r, MOVEMENT, 'goto', 1.75, -.3, True))
     s.append(Action(lambda: print('Position', r.x, r.y, r.h)))
-    s.append(RobotAction(r, MOVEMENT, 'goto', 1.2, -.2))
+    s.append(RobotAction(r, MOVEMENT, 'goto', 1.3, -.25))
     s.append(Action(lambda: print('Position', r.x, r.y, r.h)))
-    s.append(RobotAction(r, MOVEMENT, 'goto', .6, -.1))
+    s.append(RobotAction(r, MOVEMENT, 'goto', .85, -.18))
     s.append(Action(lambda: print('Position', r.x, r.y, r.h)))
-    s.append(RobotAction(r, MOVEMENT, 'goto', .1, -.1))
+    s.append(RobotAction(r, MOVEMENT, 'goto', .5, -.1))
+    s.append(RobotAction(r, MOVEMENT, 'goto', .1, -.05))
 
 
     # s.append(Action(lambda: print('Position', r.x, r.y, r.h)))
