491c2f9c8d5c4a6bf3e5cbe6dec4a3179ff8e1dd
EKnight-Blue <loicchechevalier@gmail.com>
2023-05-17 01:18:18 +0200

    tests

--- src/microProcess/no_main_process.py
+++ src/microProcess/no_main_process.py
@@ -13,11 +13,11 @@ import lidarProcess
 
 
 def lidar_process(pipe, r, color):
-    # lidar = lidarProcess.Lili()
-    # lidar.log = False
-    # lidar.lidarstop(pipe)                           #simple lidar
-    while True:
-        continue
+    lidar = lidarProcess.Lili()
+    lidar.log = False
+    lidar.lidarstop(pipe)                           #simple lidar
+    # while True:
+    #     continue
     #lidar.lidarstop2(pipe, r.x, r.y, r.h, start)   #with track limitation
 
 
