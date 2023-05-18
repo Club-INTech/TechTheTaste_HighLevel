c895ae4d02cfeb9c35e1a45b7e926f8b57fd4e21
EKnight-Blue <loicchechevalier@gmail.com>
2023-05-18 10:38:56 +0200

    tests

--- src/microProcess/no_main_process.py
+++ src/microProcess/no_main_process.py
@@ -209,11 +209,13 @@ def main_process(pipe):
     # sc = Scenario(r, pipe, RobotAction(r, ACTION, 'move_cake', LEFT, RIGHT))
 
     sc_bleu = Scenario(r, pipe, s_blue
-                  .append(RobotAction(r, MOVEMENT, 'goto', .2, 0.))
-                  .append(RobotAction(r, MOVEMENT, 'rotate', math.pi / 8))
-                  .append(RobotAction(r, MOVEMENT, 'rotate', math.pi / 8))
-                  .append(RobotAction(r, MOVEMENT, 'rotate', math.pi / 8))
-   )
+        .append(RobotAction(r, MOVEMENT, 'goto', .2, 0.))
+        .append(RobotAction(r, MOVEMENT, 'rotate', math.pi / 8))
+        .append(RobotAction(r, MOVEMENT, 'rotate', math.pi / 8))
+        .append(RobotAction(r, MOVEMENT, 'rotate', math.pi / 8))
+        .append(RobotAction(r, MOVEMENT, 'goto', .2, 0.))
+        .append(RobotAction(r, MOVEMENT, 'goto', .2, 0.))
+    )
     # sc = Scenario.test(r, pipe)
 
     sc_vert = Scenario(r, pipe, s_vert
