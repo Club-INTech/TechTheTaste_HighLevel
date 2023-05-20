caf52ca86e328e8542384bb62ccebd2375908a5a
EKnight-Blue <loicchechevalier@gmail.com>
2023-05-20 07:08:30 +0200

    Tabarnak Didier ce niaiseux

--- src/microProcess/no_main_process.py
+++ src/microProcess/no_main_process.py
@@ -272,7 +272,6 @@ def main_process(pipe):
         RobotAction(r, MOVEMENT, 'rotate', math.pi / 8),
         RobotAction(r, MOVEMENT, 'goto', .2, 0.),
         RobotAction(r, MOVEMENT, 'rotate', math.pi / 9),
-        Action(lambda: setattr(r, 'timeout_delay', .5)),
         *(RobotAction(r, MOVEMENT, 'goto', .2, 0.) for _ in range(7)),
         Action(lambda: setattr(lidarProcess.dmin, 'value', 150)),
         *(RobotAction(r, MOVEMENT, 'goto', .2, 0.) for _ in range(4)),
