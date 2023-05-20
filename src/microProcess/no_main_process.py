0790f103433bd57a56ceb3ce0f36b964b48591d9
EKnight-Blue <loicchechevalier@gmail.com>
2023-05-20 04:31:11 +0200

    symetry

--- src/microProcess/no_main_process.py
+++ src/microProcess/no_main_process.py
@@ -307,7 +307,7 @@ def main_process(pipe):
         RobotAction(r, MOVEMENT, 'goto', .2, 0.),
         RobotAction(r, MOVEMENT, 'rotate', math.pi / 8),
         Action(lambda: setattr(r, 'timeout_delay', .5)),
-        *(RobotAction(r, MOVEMENT, 'goto', .4, 0.) for _ in range(10)),
+        *(RobotAction(r, MOVEMENT, 'goto', .45, 0.) for _ in range(7)),
         Action(lambda: setattr(r, 'timeout_delay', 1.5)),
         RobotAction(r, MOVEMENT, 'rotate', -math.pi / 8),
         RobotAction(r, MOVEMENT, 'goto', .07, 0.),
